538df1352915d74d8af2613e0e28ccdd5cd3ba83
W. Perry Wortman <perrywortman@gmail.com>
2015-12-31 10:00:49 -0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp       = require('gulp');
+var beep       = require('beepbeep')
+var gutil      = require('gulp-util');
+var plumber    = require('gulp-plumber');
+var uglify     = require('gulp-uglifyjs');
+var sass       = require('gulp-ruby-sass');
+var livereload = require('gulp-livereload');
+
+var onError = function (err) {
+    beep([0, 0, 0]);
+    gutil.log(gutil.colors.green(err));
+};
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+//////////     WEBSITE TASKS
+//////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// JS
+gulp.task('uglifyjs', function() {
+    return gulp.src([
+        './js/boxlayout.js',
+        './js/modernizr.custom.js'
+    ])
+    .pipe(plumber({
+        errorHandler: onError
+    }))
+    .pipe(uglify('app.js', {
+        compress: true
+    }))
+    .pipe(gulp.dest('./js/'))
+    .pipe(livereload());
+});
+
+// Sass
+gulp.task('sass', function() {
+    return sass([
+        './scss/app.scss'
+    ])
+    .pipe(plumber({
+        errorHandler: onError
+    }))
+    .pipe(gulp.dest('./css/'))
+    .pipe(livereload());
+});
+
+// HTML
+gulp.task('html', function() {
+    return gulp.src([
+        './index.html'
+    ])
+    .pipe(livereload());
+});
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+//////////     WATCH AND BUILD TASKS
+//////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Primary task to watch other tasks
+gulp.task('yo', function() {
+    // LiveReload
+    livereload.listen();
+
+    // Watch JS
+    gulp.watch(['./js/app.js', './js/boxlayout.js'], ['uglifyjs']);
+
+    // Watch Sass
+    gulp.watch(['./scss/_styles.scss', './scss/app.scss'], ['sass']);
+
+    // Watch HTML and livereload
+    gulp.watch('./index.html', ['html']);
+});
+
+// Manually build all
+gulp.task('build', function() {
+    gulp.start('uglifyjs', 'sass');
+});
